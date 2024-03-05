038ed7ead63485e8677f948cbb74f0ae5fe37ece
ramla02 <ramla252017@gmail.com>
2024-03-05 20:53:08 +0300

    hello worls

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - the main function is the start of the program.
+* C program that prints "Hello World" using function.
+* Return: 0 for the successful execution.
+* source: https://www.sanfoundry.com/c-program-hello-world/
+*/
+void print()
+{
+printf("Hello, World\n");
+}
+void main()
+{
+/* calling the function print */
+print();
+}
